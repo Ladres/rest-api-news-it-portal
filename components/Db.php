26065a0b15baa4a3ade36f1fdbbc3b703de6fdf6
Ladres <Sanyokdonetruuu@yandex.ru>
2017-12-22 15:38:30 +0400

    #2 Change connection file

--- components/Db.php
+++ components/Db.php
@@ -4,10 +4,11 @@ class DB
 	public static function getConnection()
 	{
 
-		$host = 'localhost'; 
-		$user = 'root'; 
-		$password = ''; 
-		$db_name = 'mvc_site'; 
+		$host = 'us-cdbr-iron-east-05.cleardb.net'; 
+		$user = 'ba87b64b655102'; 
+		$password = '8f176f15'; 
+		$db_name = 'heroku_e8f598c73c2b616';  
+		  
 		
 		$link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
 		mysqli_query($link, "SET NAMES 'utf8'");
